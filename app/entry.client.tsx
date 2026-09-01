0491b10c2329f65ccf948eb6addfe1f66fa12c38
Seyed Hoseyn <hoseyn.mousavi78@gmail.com>
2026-09-01 22:04:25 +0330

    added service worker

---
+++ app/entry.client.tsx
@@ -0,0 +1,15 @@
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import { HydratedRouter } from "react-router/dom";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW();
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <HydratedRouter />
+    </StrictMode>,
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import {reactRouter} from "@react-router/dev/vite"
+import tailwindcss from "@tailwindcss/vite"
+import {defineConfig} from "vite"
+import {VitePWA} from "vite-plugin-pwa"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-});
+    plugins: [
+        tailwindcss(),
+        reactRouter(),
+        VitePWA({
+            injectRegister: "inline",
+            manifest: false,
+            outDir: "build/client",
+            registerType: "autoUpdate",
+            workbox: {
+                globPatterns: ["assets/**/*.{js,css}"],
+                navigateFallback: null,
+                runtimeCaching: [],
+            },
+        }),
+    ],
+    resolve: {
+        tsconfigPaths: true,
+    },
+})
